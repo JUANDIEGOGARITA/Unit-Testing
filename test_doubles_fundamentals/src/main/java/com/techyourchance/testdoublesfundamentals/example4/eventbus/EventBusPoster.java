4d6788e7e75c9c95443175ad74862e461af58b66
JD <juandiegogarita@gmail.com>
2020-09-04 22:23:57 -0600

    completes exercise 4

--- test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/example4/eventbus/EventBusPoster.java
+++ test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/example4/eventbus/EventBusPoster.java
@@ -3,5 +3,4 @@ package com.techyourchance.testdoublesfundamentals.example4.eventbus;
 public interface EventBusPoster {
 
   void postEvent(Object event);
-
 }
--- test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/example5/ServerUsernameValidator.java
+++ test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/example5/ServerUsernameValidator.java
@@ -14,5 +14,4 @@ public class ServerUsernameValidator {
       return false;
     }
   }
-
 }
